372dbea30079c40d5569dc17f2cd1218f7e9c8c1
vishal086 <vishalkumar359877@gmail.com>
2024-09-08 16:05:38 +0530

    final change

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
 const bodyParser = require('body-parser')
 const {Register} = require('./models/complainstModel')
 const { connectDB } = require('./config/connection')
@@ -10,8 +10,6 @@ app.use(express.json())
 
 // app.use(cors())
 
-app.use(cors());
-
 app.use(function (req, res, next) {
     //Enabling CORS
     res.header("Access-Control-Allow-Origin", "*");
@@ -19,6 +17,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
+    
 app.use(bodyParser.json())
 
 
