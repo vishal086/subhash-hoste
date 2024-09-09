f9a8497bd1a26c0a418328081eea6997a0a1cc40
vishal086 <vishalkumar359877@gmail.com>
2024-09-09 11:39:51 +0530

    change

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 // const URL = "mongodb://127.0.0.1:27017/Shivam"
-const URI = "mongodb+srv://vishalkumar03072001:BIe3wUY7b073jkkD@cluster0.4mqrp.mongodb.net/Subhash?retryWrites=true&w=majority&appName=Cluster0"
+// const URI = "mongodb+srv://vishalkumar03072001:BIe3wUY7b073jkkD@cluster0.4mqrp.mongodb.net/Subhash?retryWrites=true&w=majority&appName=Cluster0"
 
+
+const URI = "mongodb+srv://hsrsrr371:CHAz4cgnZ0zsJHdK@cluster0.eo543.mongodb.net/Subhash-Hostel?retryWrites=true&w=majority&appName=Cluster0"
 const connectDB = async() =>{
     try {
         mongoose.connect(URI)
         console.log("DB Connected")
     } catch(err) {
-        console.log("Connection Failed")
+        console.log("Connection Failed") 
     }
 }
 
